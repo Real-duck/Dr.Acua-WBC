b9720e1f8fc4148da6bd122ce6865315cc495b52
Real-duck <89462645+Real-duck@users.noreply.github.com>
2022-10-17 22:53:32 +0200

    Update home.py

--- home.py
+++ home.py
@@ -22,7 +22,7 @@ import tensorflow as tf
 tf.compat.v1.disable_eager_execution()
 
 def home():
-    st.warning('Da viele Personen diese Website benutzt haben, verlangsamt Streamlit diese! Dies liegt – glaube ich – an Uploads, da Streamlit keinen endlosen temporären Speicherplatz bereitstellt. Wenn die Seite einen Error hat, bitte eine E-Mail an david-linus.rutkevich@ueg-leer.net schreiben') 
+    st.warning('Weil Streamlit die Websie verlangsammt, ist das Plättchen-Modul gesperrt worden.') 
     st.markdown('## Was ist Dr Acula?')
     st.markdown('Inspiriert vom berüchtigten Vampir - mit seiner eigenen Netflix Serie - Dracula, hat sich '
                'der Vampir Dr. Acula dazu entschieden sein Wissen über Blut zu veröffentlichen. Er kann '
